4fb2ea08a5239c7bf0bef19a272f53ff39ca0f24
aamir221 <aamirmanzoor372@gmail.com>
2024-11-20 10:55:01 +0530

    added repo files

---
+++ hotpot/src/main/java/com/hexaware/hotpot/repository/MenuItemRepository.java
@@ -0,0 +1,11 @@
+package com.hexaware.hotpot.repository;
+
+import com.hexaware.hotpot.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<User, Long> {
+    
+}
